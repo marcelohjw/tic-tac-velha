b4e134d1f39dea1eeecfb036e540cc56a78509e9
Marcelo Junior <marcelohjw@gmail.com>
2023-03-17 22:59:32 -0300

    All game logic to change cross and circles done

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ const gameboard = document.querySelector( '#gameboard');
 const info = document.querySelector('#info');
 const startCells = ["", "", "", "", "", "", "", "", ""];
 
+let go = "circle";
+let texto = "círculo";
+info.textContent = "Círculos Primeiro";
+
 function createBoard() {
     startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
@@ -16,6 +20,11 @@ createBoard();
 
 function addGo(e) {
     const goDisplay = document.createElement('div');
-    goDisplay.classList.add('cross');
+    goDisplay.classList.add(go);
     e.target.append(goDisplay);
+    go = go === "circle" ? "cross" : "circle";
+    texto = texto === "círculo" ? "cruz" : "círculo";
+    
+    info.textContent = " agora é " + texto + ".";
+    e.target.removeEventListener("click", addGo);
 }
